9770c6bd8027ab4334f2bebbf8dd94768ce1630c
Joseph Shunk <joseph.shunk@gmail.com>
2023-11-26 02:14:35 -0800

    Added a block

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -9,7 +9,7 @@ int main()
 	c_game game{ 1280, 720, "Game" };
 	c_rocket const& rocket = game.state().rocket();
 	game.sprites().make_sprite("rocket"_h, "data/rocket.png", 0.f, 0.f);
+	game.sprites().make_sprite("block"_h, "data/block.jpg", 0.f, 0.f);
 	game.run();
-
 	return 0;
 }
