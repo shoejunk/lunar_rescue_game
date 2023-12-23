6bd16fbebfa593d9dbd4550439cef12a5da5c0e8
Joseph Shunk <joseph.shunk@gmail.com>
2023-12-22 23:27:46 -0800

    Switch to latest main for libs. Minor code cleanup.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -12,20 +12,14 @@ int main()
 	c_rocket const& rocket = game.state().rocket();
 	game.sprites().make_sprite("rocket"_h, "data/rocket_small.png", 0.f, 0.f);
 	game.sprites().make_sprite("block"_h, "data/block_small.jpg", 64.f, 64.f);
-
-	sf::Sprite* sprite = game.sprites().get("block"_h);
-
+	sf::Sprite* block_sprite = game.sprites().get("block"_h);
 	sf::Sprite* rocket_sprite = game.sprites().get("rocket"_h);
 	sf::Texture const* rocket_texture = rocket_sprite->getTexture();
-	sf::Color color = rocket_texture->copyToImage().getPixel(70, 64);
-	color = rocket_texture->copyToImage().getPixel(41, 56);
 	auto texture_size = rocket_texture->getSize();
-	c_collision_mask mask(texture_size.x, texture_size.y);
-	mask.from_image(rocket_texture->copyToImage());
-
-	if (sprite != nullptr)
+	c_collision_mask mask(rocket_texture->copyToImage());
+	if (block_sprite != nullptr)
 	{
-		sprite->setOrigin(sf::Vector2f(sprite->getTexture()->getSize()) / 2.f);
+		block_sprite->setOrigin(sf::Vector2f(block_sprite->getTexture()->getSize()) / 2.f);
 	}
 	game.run();
 	return 0;
