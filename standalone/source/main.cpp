5085ef5bb9d3efa679ef6e857ba500cd2b43cef9
Joseph Shunk <joseph.shunk@gmail.com>
2023-12-27 12:40:35 -0800

    Add an option to scale the image when using it as a collision mask.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,15 +1,21 @@
+// Imports
 import lunar_rescue.game;
 import std.core;
 import stk.hash;
 import stk.collide;
 import stk.log;
 
+// Namespaces
 using namespace lunar_rescue;
 using namespace stk;
 
+// Main
 int main()
 {
+	// Set up the game.
 	c_game game{ 1280, 720, "Game" };
+
+	// Set up the sprites.
 	sf::Sprite* rocket_sprite = game.sprites().make_sprite("rocket"_h, "data/rocket_small.png", 0.f, 0.f);
 	sf::Sprite* block_sprite = game.sprites().make_sprite("block"_h, "data/block_small.jpg", 64.f, 64.f);
 	if (rocket_sprite == nullptr)
@@ -20,17 +26,25 @@ int main()
 	{
 		errorln("Failed to make sprite for block.");
 	}
+	block_sprite->setOrigin(sf::Vector2f(block_sprite->getTexture()->getSize()) / 2.f);
+
+	// Assign the rocket and block their images.
 	sf::Texture const* rocket_texture = rocket_sprite->getTexture();
 	if (rocket_texture == nullptr)
 	{
 		errorln("Failed to get texture for rocket sprite.");
 	}
-	auto texture_size = rocket_texture->getSize();
 	game.state().rocket().image(*rocket_texture);
-	if (block_sprite != nullptr)
+	sf::Texture const* block_texture = block_sprite->getTexture();
+	if (block_texture == nullptr)
 	{
-		block_sprite->setOrigin(sf::Vector2f(block_sprite->getTexture()->getSize()) / 2.f);
+		errorln("Failed to get texture for block sprite.");
 	}
+	game.state().block().image(*block_texture);
+
+	// Run the game.
 	game.run();
+
+	// Exit successfully.
 	return 0;
 }
